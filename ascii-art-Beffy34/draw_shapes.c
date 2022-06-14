06c14955191030ad15ee4f18831a5ea707421493
Beffy34 <107278569+Beffy34@users.noreply.github.com>
2022-06-14 12:18:30 -0600

    Add files via upload

---
+++ ascii-art-Beffy34/draw_shapes.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include "draw.h"
+
+/* Prints a size x size square whose left col is at startCol */
+void print_square(int leftCol, int size)
+{
+  int endCol = leftCol + size;
+  for (int row = 0; row < size; row++){
+    int col;
+    for (col = 0; col < leftCol; col++) putchar(' ');
+    for (       ; col < endCol;  col++) putchar('*');
+    putchar('\n');
+  }
+}
+
+// Prints a triangle of specified height whose left edge is at col leftCol.
+void print_triangle(int leftCol, int size)
+{
+  for (int row = 0; row <= size; row++) {
+    int minCol = leftCol + size - row, maxCol = leftCol + size + row;
+    int col;
+    for (col = 0; col < minCol; col++) putchar(' ');
+    for (       ; col <= maxCol; col++) putchar('*');
+    putchar('\n');
+  }
+}
+
+// Prints a arrow of a specified height whose left edge is at col lefCol.
+void print_arrow(int leftCol, int size)
+{
+    for (int row = 0; row <= size; row++) {
+      int minCol = leftCol + size - row, maxCol = leftCol + size + row;
+      int col;
+      for (col = 5; col < minCol; col++) putchar(' ');
+      for (       ; col <= maxCol; col++) putchar('*');
+      putchar('\n');
+    }
+    int endCol = leftCol + size;
+    for (int row = 0; row < size; row++){
+      int col;
+      for (col = 0; col < leftCol; col++) putchar(' ');
+      for (       ; col < endCol;  col++) putchar('*');
+      putchar('\n');
+    }
+
+}
---
+++ ascii-art-Beffy34/uimain.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include "draw.h"
+
+int main() 
+{
+  puts("Welcome!");
+
+  while (1) { // Infinite while loop
+
+    fputs("Select which shape you want to print (Arrow = a, Triangle = t, Square = s, Chars = c, OtherChar = d) or 'q' to quit\n> ", stdout);
+    fflush(stdout);		/* stdout only flushes automatically on \n */
+    int c;
+    while ((c = getchar()) == '\n'); /* ignore newlines */
+    if (c == EOF)		     /* terminate on end-of-file */
+      goto done;
+
+    // Given the user answer, select which method to call
+    switch (c) {
+    case 't':
+      puts("You selected triangle:");
+      print_triangle(5, 7);
+      break;
+    case 's':
+      puts("You selected square:");
+      print_square(5, 5);
+      break;
+    case 'c':
+      puts("You selected chars:");
+      for (char c = 'A'; c < 'D'; c++)
+	print_char_11x16(c);
+      break;
+    case 'd':
+      puts("You selected other chars:");
+      for (char d = 'A'; d < 'D'; d++)
+    print_char_8x12(d);
+      break;
+    case 'a':
+      puts("You selected arrow:");
+      print_arrow(5, 10);
+      break;
+    case 'q':
+      puts("Bye!");
+      goto done; 		/* terminate */
+    case '\n':
+      break;
+    default:
+      printf("Unrecognized option '%c', please try again!\n", c);
+    }
+  }
+
+ done: // To exit from program
+  return 0;
+}
